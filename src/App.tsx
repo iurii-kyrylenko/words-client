dc5daa41c406bef069785f5ef9f06785c12e1f23
iurii-kyrylenko <yuriproject@gmail.com>
2024-07-02 22:32:34 +0200

    Initial markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
-}
+import Ex01 from "./components/Ex01";
 
-export default App
+export default function App() {
+    return (
+        <div className="h-screen flex flex-col">
+            <div className="bg-blue-600 dark:bg-slate-800 h-16 flex-none"></div>
+            <div className="bg-yellow-200 dark:bg-slate-600 flex-grow">
+                <Ex01 />
+            </div>
+        </div>
+    );
+}
