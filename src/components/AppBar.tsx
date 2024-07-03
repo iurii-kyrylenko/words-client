707d50428225fef5325a7159cadef11769690753
iurii-kyrylenko <yuriproject@gmail.com>
2024-07-03 20:48:26 +0200

    Support dark mode

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,18 +1,46 @@
+import { useEffect, useState } from "react";
+import { Button } from "@headlessui/react";
+
 export default function AppBar() {
+    const [isDark, setDark] = useState(false);
+
+    const changeMode = () => {
+        setDark(!isDark);
+    };
+
+    useEffect(() => {
+        const documentClasses = document.documentElement.classList;
+        if (isDark) {
+            documentClasses.add("dark");
+        } else {
+            documentClasses.remove("dark");
+        }
+    }, [isDark]);
+
     return (
         <nav className="relative flex gap-4 flex-wrap items-center px-4 py-2 lg:py-4 shadow-md bg-zinc-50 dark:bg-neutral-700 dark:text-white">
             <div className="flex-0">
                 <div className="flex items-center">
                     <img
                         className="mr-2 h-10"
-                        src="https://cdn-icons-png.flaticon.com/512/8541/8541712.png"
+                        src="https://cdn-icons-png.flaticon.com/512/8541/8541715.png"
                         alt="Words Logo"
                         loading="lazy" />
                     <span className="font-semibold">Words</span>
                 </div>
             </div>
-            <div className="flex-1 hover:underline">Hurdle</div>
-            <div className="flex-0 hover:underline">Example</div>
+            <div className="flex-1">Hurdle Helper</div>
+            <Button onClick={changeMode}>
+                {isDark ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                    </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                    </svg>
+                )}
+            </Button>
         </nav>
     );
 }
