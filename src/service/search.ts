9020f6be434e537b160405a98c47294b6968fb86
iurii-kyrylenko <yuriproject@gmail.com>
2024-08-08 20:25:05 +0200

    Simplify picking preset option

--- src/service/search.ts
+++ src/service/search.ts
@@ -20,17 +20,18 @@ export const search = (wordSize: number, wordInfos: WordInfo[]) => {
     const remains = filterOutUsedAndDups(ratedWords, wordInfos)
         .map((rw) => rw.word);
     const options = filterPresetOptions(presetOptions, wordInfos);
-    const option = pickOption(options, matches);
+    // const option = pickOption(options, matches);
+    const option = options[0];
     return { remains, matches, option };
 };
 
-const isIntersect = (option: string, match: string) =>
-    [...option].some((pchar) => [...match].some((mchar) => pchar === mchar));
+// const isIntersect = (option: string, match: string) =>
+//     [...option].some((pchar) => [...match].some((mchar) => pchar === mchar));
 
-const rateOption =  (option: string, matches: string[]) =>
-    matches.reduce((count, match) => isIntersect(option, match) ? count + 1 : count, 0);
+// const rateOption =  (option: string, matches: string[]) =>
+//     matches.reduce((count, match) => isIntersect(option, match) ? count + 1 : count, 0);
 
-const pickOption = (options: string[], matches: string[]) =>
-    options
-        .map((option) => ({ option, rate: rateOption(option, matches) }))
-        .sort((a, b) => b.rate - a.rate)[0]?.option;
+// const pickOption = (options: string[], matches: string[]) =>
+//     options
+//         .map((option) => ({ option, rate: rateOption(option, matches) }))
+//         .sort((a, b) => b.rate - a.rate)[0]?.option;
