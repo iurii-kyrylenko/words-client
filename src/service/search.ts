e94c1df5754c37a335c68e2a3155ba5f0d727c2d
iurii-kyrylenko <yuriproject@gmail.com>
2024-08-17 22:53:48 +0200

    Use minmax to pick an option

--- src/service/search.ts
+++ src/service/search.ts
@@ -13,6 +13,9 @@ import allWords from "./words-rated.json";
 type Presets = { [size: number]: string[] };
 
 export const search = (wordSize: number, wordInfos: WordInfo[], answers: string[]) => {
+    if (wordSize !== 5) {
+        answers = [];
+    }
     const presetOptions = (presets as Presets)[wordSize];
     const ratedWords = filterSize(allWords as RatedWord[], wordSize);
     const matches = filterWordInfos(ratedWords, wordInfos)
@@ -20,12 +23,42 @@ export const search = (wordSize: number, wordInfos: WordInfo[], answers: string[
         .map((rw) => rw.word);
     const remains = filterOutUsedAndDups(ratedWords, wordInfos)
         .map((rw) => rw.word);
-    const option = filterPresetOptions(presetOptions, wordInfos)[0];
 
-    if (matches.length > 2 && matches.length < 301) {
-        const mmQuestions = getMinMaxQuestions(answers, matches);
-        console.log(mmQuestions);
+    const option = pickOption({ presetOptions, wordInfos, matches, answers });
+    return { remains, matches, option };
+};
+
+interface PickOptionParams {
+    presetOptions: string[];
+    wordInfos: WordInfo[];
+    matches: string[];
+    answers: string[];
+}
+
+const pickOption = ({ presetOptions, wordInfos, matches, answers }: PickOptionParams) => {
+    const threshold = 84;
+    const answersSet = new Set(answers);
+    const matchedAnswers = matches.filter((w) => answersSet.has(w));
+    const matchedAnswersLength = matchedAnswers.length;
+    const matchedLength = matches.length;
+
+    if (wordInfos.length === 0) {
+        return filterPresetOptions(presetOptions, wordInfos)[0];
     }
 
-    return { remains, matches, option };
+    if (matchedAnswersLength === 1 || matchedAnswersLength === 2) {
+        return matchedAnswers[0];
+    }
+
+    if (matchedAnswersLength === 0) {
+        return matchedLength > threshold
+            ? filterPresetOptions(presetOptions, wordInfos)[0]
+            : matchedLength < 3
+            ? matches[0]
+            : getMinMaxQuestions(answers, matches).questions[0];
+    }
+
+    return matchedAnswersLength > threshold
+        ? filterPresetOptions(presetOptions, wordInfos)[0]
+        : getMinMaxQuestions(answers, matchedAnswers).questions[0];
 };
